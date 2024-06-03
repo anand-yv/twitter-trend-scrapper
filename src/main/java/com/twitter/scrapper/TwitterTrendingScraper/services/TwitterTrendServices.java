b40c9dcec1401501affd3f56c18d13972a5c55f5
Anand Yadav <anand93056ad@gmail.com>
2024-06-03 11:39:06 +0530

    Aded

--- src/main/java/com/twitter/scrapper/TwitterTrendingScraper/services/TwitterTrendServices.java
+++ src/main/java/com/twitter/scrapper/TwitterTrendingScraper/services/TwitterTrendServices.java
@@ -169,9 +169,9 @@ public class TwitterTrendServices {
         }
 
         private List<String> top5Trends(WebDriverWait wait) {
-                String xpathPattern = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[4]/div/section/div/div/div[%d]/div/div/div/div[2]";
+                String xpathPattern = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/section/div/div/div[%d]/div/div/div/div[2]";
                 List<String> trendingDivs = new ArrayList<>();
-                for (int i = 3; i < 8; i++) {
+                for (int i = 4; i < 9; i++) {
                         String xpath = String.format(xpathPattern, i);
 
                         try {
